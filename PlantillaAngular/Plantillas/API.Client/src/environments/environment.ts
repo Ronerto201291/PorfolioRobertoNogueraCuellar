5e3723822d1fc9d48e6b02952a129e28621ab366
Roberto Noguera Cuellar <rnoguera@valumre.com>
2026-01-26 12:59:21 +0100

    porfolio

--- PlantillaAngular/Plantillas/API.Client/src/environments/environment.ts
+++ PlantillaAngular/Plantillas/API.Client/src/environments/environment.ts
@@ -6,24 +6,5 @@ export const environment  = {
   
 };
 
-//export const ElkConfiguration: IELKEnvironment = {
-//    production: true,
-//    appName: 'MGA-Angular',
-//    endpoints: {
-//        elasticSearchEndpoint: 'http://[ip publica ELK]:[puerto]/_bulk'
-//    },
-//    ApmElkConfig: {
-//      INDEX_NAME: 'MGA-Smaple-Angular',
-//      APP_FIELD: 'MGA-Smaple-Angular',
-//        ENV_FIELD: 'Produccion',
-//        VERSION_FIELD: 'Version',
-//        USER_NAME_FIELD: 'UserName',
-//        ELAPSED_MS_FIELD: 'ElapsedMilliseconds',
-//        REQUEST_PATH_FIELD: 'RequestPath',
-//        URL_FIELD: 'Url',
-//        APP_STATE_FIELD: 'AppState',
-//    },
-//    ProxyTokenHeaderName: "proxyElkToken",
-//    env: "pro"
-//};
+
 
